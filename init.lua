f98ee630867f1aff88a1da33858d05c5c0ed2912
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2016-11-27 08:45:53 +0900

    Add lua file

---
+++ init.lua
@@ -0,0 +1,54 @@
+require('watcher')
+
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+   end
+end
+
+local function keyCodeSet(keys)
+   return function()
+      for i, keyEvent in ipairs(keys) do
+         keyEvent()
+      end
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+      -- hs.alert.show(name)
+      if name == "iTerm2" then
+         disableAllHotkeys()
+      else
+         enableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+remapKey({'ctrl'}, 'l', keyCode('right'))
+remapKey({'ctrl'}, 'h', keyCode('left'))
+remapKey({'ctrl'}, 'j', keyCode('down'))
+remapKey({'ctrl'}, 'k', keyCode('up'))
+
