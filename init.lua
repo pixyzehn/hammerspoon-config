82c979e9642af961fa050c3d7319ff34c686b8c8
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-01-02 20:49:45 +0900

    Update handleGlobalAppEvent func

--- init.lua
+++ init.lua
@@ -36,11 +36,7 @@ end
 local function handleGlobalAppEvent(name, event, app)
    if event == hs.application.watcher.activated then
       -- hs.alert.show(name)
-      if name == "iTerm2" then
-         disableAllHotkeys()
-      else
-         enableAllHotkeys()
-      end
+      enableAllHotkeys()
    end
 end
 
